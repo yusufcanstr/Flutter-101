8a26d507012c42219ef54be225397b1071e91477
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-12-01 20:36:29 +0300

    color learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:information_card/101/color_learn.dart';
 import 'package:information_card/101/icon_learn.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       )),
-      home: const IconViewLearn(),
+      home: const ColorLearn(),
     );
   }
 }
