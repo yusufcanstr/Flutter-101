114f72a11284977643d1c59f6a3cc8f65a128a45
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-12-17 22:34:46 +0300

    navigation learn and apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'demos/my_collections_demos.dart';
+import '101/navigation_learn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0,
           )),
-      home: const MyCollectionsDemos(),
+      home: const NavigationLearn(),
     );
   }
 }
